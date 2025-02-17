fcf7cbcfc492a0a8a91ba08183b4e28a0dcdbd7a
higor-leodoro <higor.leodoro@gmail.com>
2025-02-17 17:13:23 -0300

    feat: game phases broken logic

--- src/app/useViewModel.tsx
+++ src/app/useViewModel.tsx
@@ -1,22 +1,42 @@
 import { useRef, useState } from "react";
 import { ReactDiceRef } from "react-dice-complete";
 
+enum GamePhase {
+  ComeOutRoll = "Come Out Roll",
+  PointPhase = "Point Phase",
+  EndGame = "End game",
+}
+
 export default function useViewModel() {
   const reactDice = useRef<ReactDiceRef>(null);
   const [result, setResult] = useState("");
+  const [gamePhase, setIsGamePhase] = useState<GamePhase>(
+    GamePhase.ComeOutRoll
+  );
+  const [point, setPoint] = useState(0);
 
   const handleRollDone = (total: number) => {
     const pointers = [4, 5, 6, 8, 9, 10];
 
-    if (result === "Ponto") {
-    }
-
-    if (total === 7 || total === 11) {
-      return setResult("Viótia natural");
-    } else if (total === 2 || total === 3 || total === 12) {
-      return setResult("Craps");
-    } else if (pointers.includes(total)) {
-      return setResult("Ponto");
+    if (gamePhase === GamePhase.PointPhase) {
+      if (total === 7) {
+        setResult("Pedeu");
+      } else if (total === point) {
+        setResult("Vitória");
+        return alert("fim de jogo");
+      }
+    } else if (gamePhase === GamePhase.ComeOutRoll) {
+      if (total === 7 || total === 11) {
+        return setResult("Viótia natural");
+      } else if (total === 2 || total === 3 || total === 12) {
+        return setResult("Craps");
+      } else if (pointers.includes(total)) {
+        setResult("Ponto");
+        setPoint(total);
+        setIsGamePhase(GamePhase.PointPhase);
+      }
+    } else {
+      setResult("Fim de jogo");
     }
   };
 
