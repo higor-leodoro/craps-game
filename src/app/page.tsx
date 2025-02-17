1c29548fa8cf59ee9b3cff878b9b33d820d35e59
higor-leodoro <higor.leodoro@gmail.com>
2025-02-17 16:53:49 -0300

    feat: new dice lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,27 @@
 "use client";
 
-import { useState } from "react";
-import Dice from "react-dice-roll";
+import ReactDice from "react-dice-complete";
 import useViewModel from "./useViewModel";
 
 export default function Home() {
-  const { rollDice } = useViewModel();
-  const [diceValue, setDiceValue] = useState(rollDice());
-
-  const handleRoll = () => {
-    setDiceValue(rollDice());
-  };
-
-  const value = rollDice();
-  console.log(value);
-
+  const { handleRollDone, reactDice, handleRollDice, result } = useViewModel();
   return (
     <main className="flex flex-col justify-center items-center h-screen">
       <div className="flex gap-20">
-        <Dice cheatValue={diceValue} />
+        <ReactDice
+          numDice={2}
+          ref={reactDice}
+          rollDone={handleRollDone}
+          defaultRoll={1}
+        />
       </div>
       <div className="mt-10">
-        <button className="bg-red-400 p-4 rounded" onClick={handleRoll}>
-          <h1 className="font-bold text-white text-xl">Roll Dice</h1>
+        <button className="bg-red-400 p-4 rounded" onClick={handleRollDice}>
+          <h1 className="font-bold text-white text-xl">Roll dices</h1>
         </button>
+      </div>
+      <div>
+        <h1>{result}</h1>
       </div>
     </main>
   );
--- src/app/useViewModel.tsx
+++ src/app/useViewModel.tsx
@@ -1,8 +1,28 @@
-type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
+import { useRef, useState } from "react";
+import { ReactDiceRef } from "react-dice-complete";
 
 export default function useViewModel() {
-  const rollDice = (): DiceValue =>
-    (Math.floor(Math.random() * 6) + 1) as DiceValue;
+  const reactDice = useRef<ReactDiceRef>(null);
+  const [result, setResult] = useState("");
 
-  return { rollDice };
+  const handleRollDone = (total: number) => {
+    const pointers = [4, 5, 6, 8, 9, 10];
+
+    if (result === "Ponto") {
+    }
+
+    if (total === 7 || total === 11) {
+      return setResult("Viótia natural");
+    } else if (total === 2 || total === 3 || total === 12) {
+      return setResult("Craps");
+    } else if (pointers.includes(total)) {
+      return setResult("Ponto");
+    }
+  };
+
+  const handleRollDice = () => {
+    reactDice.current?.rollAll();
+  };
+
+  return { handleRollDone, reactDice, handleRollDice, result };
 }
